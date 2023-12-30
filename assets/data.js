055461b10258fec6d67c4bf874ae19f18f774b26
Dzubayyan Ahmad <masdzub@gmail.com>
2023-12-30 14:49:52 +0700

    add modified much and make more better

--- assets/data.js
+++ assets/data.js
@@ -1,6 +1,3 @@
-
-
-// script.js
 new Vue({
     el: '#app',
     data: {
@@ -41,7 +38,24 @@ new Vue({
           companyLink: 'https://domainesia.com',
           startDate: '2022/10',
           endDate: 'Present',
-          description: 'Render technical advice, support, and assistance to various organizations and consumers for technical and software problems, also deal with end-users directly and effectively communicate answers in an understandable manner.'
+          descriptionItems: [
+            'Developed and implemented technical support processes and procedures to enhance efficiency and effectiveness.',
+            'Monitored system and procedural processes encompassing Abuse, SSL, migration, cPanel license, and other issues.',
+            'Collaborate with cross-functional teams, including infrastructure and product management (sales), to identify and resolve technical issues.',
+            'Analyze and resolve complex technical issues, utilizing resources such as documentation and internal systems.',
+            'Maintain a high level of customer service and support for all technical issues, including troubleshooting and problem resolution.',
+            'Migrate account hosting to balancing resource and migration from outside into our server and new server.',
+            'Mentor technical support staff on technical troubleshooting and problem-solving techniques.',
+            'Coordinate with third parties to resolve problem issues in the event of an error.',
+            'Assist the manager in recruiting new staff and training new staff.',
+            'Handled customer inquiries and technical issues through ticketing systems, and phone, providing prompt and accurate solutions to customer problems.',
+            'Diagnose and troubleshoot errors and technical glitches in client applications or systems. Effectively fixing issues to ensure client satisfaction and minimal downtime.',
+            'Articulate technical product details to new customers clearly and understandably helping customers grasp the value and functionality of our services.',
+            'Provide pre-sales support to prospective clients, address technical inquiries and concerns, and assist potential customers in making informed decisions about our hosting services.',
+            'Implemented rigorous security measures, including screening for domain and hosting abuse such as fraud, phishing, spamming, and other illicit activities, ensuring a secure hosting environment for clients.',
+          ],
+          employmentStatus: "Permanent",
+          workStatus: "Hybrid" 
         },
         {
           title: 'Technical Support Specialist',
@@ -49,7 +63,14 @@ new Vue({
           companyLink: 'https://domainesia.com',
           startDate: '2019/03',
           endDate: '2022/10',
-          description: 'Render technical advice, support, and assistance to various organizations and consumers for technical and software problems, also deal with end-users directly and effectively communicate answers in an understandable manner.'
+          descriptionItems: [
+            'Handled customer inquiries and technical issues through ticketing systems, live chat, and phone, providing prompt and accurate solutions to customer problems.',
+            'Diagnose and troubleshoot errors and technical glitches in client applications or systems. Effectively fixing issues to ensure client satisfaction and minimal downtime.',
+            'Articulate technical product details to new customers clearly and understandably helping customers grasp the value and functionality of our services.',
+            'Provide pre-sales support to prospective clients, address technical inquiries and concerns, and assist potential customers in making informed decisions about our hosting services.',
+          ],
+          employmentStatus: "Permanent",
+          workStatus: "On-Site" 
         },
         {
           title: 'System Administrator',
@@ -57,7 +78,13 @@ new Vue({
           companyLink: '#',
           startDate: '2020/04',
           endDate: '2021/13',
-          description: 'Manage and maintenance server infrastructure and our server client.'
+          descriptionItems: [
+            'Deployed, managed, and maintained dozens of production servers, ensuring optimal performance and stability.',
+            'Implemented robust server monitoring systems to proactively track the health and performance of all production servers, thereby ensuring uninterrupted service and guaranteeing minimal downtime.',
+            'Enhanced our server services by automating routine tasks and deploying new features with automation tools, improving operational efficiency.',
+          ],
+          employmentStatus: "Self-Employee",
+          workStatus: "Hybrid" 
         },
         {
           title: 'Linux System Administrator',
@@ -65,7 +92,13 @@ new Vue({
           companyLink: 'https://terascode.com',
           startDate: '2018/07',
           endDate: '2019/06',
-          description: 'Managed daily activities to include user support and system administration task, Managed the day to day IT troubleshooting for users both locally and remotely.'
+          descriptionItems: [
+            'Deployed, managed, and maintained dozens of production servers, ensuring optimal performance and stability.',
+            'Implemented robust server monitoring systems to proactively track the health and performance of all production servers, thereby ensuring uninterrupted service and guaranteeing minimal downtime.',
+            'Enhanced our server services by automating routine tasks and deploying new features with automation tools, improving operational efficiency.',
+          ],
+          employmentStatus: "Permanent",
+          workStatus: "Hybrid" 
         },
         {
           title: 'Training Development Staff',
@@ -73,7 +106,14 @@ new Vue({
           companyLink: 'https://terascode.com',
           startDate: '2018/04',
           endDate: '2019/16',
-          description: 'Identify training and development needs within an organisation through job analysis, appraisal schemes and regular consultation with business managers and human resources departments.'
+          descriptionItems: [
+            'Develop training materials, modules, and curriculum based on identified needs, learning objectives, and organizational goals. This involves creating content, presentations, manuals, and other educational materials.',
+            'Identify and assess the training needs of the organization or specific departments through surveys, interviews, or analysis of performance data.',
+            'Work closely with subject matter experts, trainers, and other stakeholders to gather information and ensure accuracy and relevance of training materials.',
+            'Stay updated with industry trends, best practices, and new technologies in training and development to enhance the quality and effectiveness of training programs.',
+          ],
+          employmentStatus: "Permanent",
+          workStatus: "Hybrid" 
         },
         {
           title: 'Assistant Lecture',
@@ -81,7 +121,16 @@ new Vue({
           companyLink: 'https://amikom.ac.id',
           startDate: '2017/09',
           endDate: '2018/07',
-          description: 'Supervise the work of students, provide advice on study skills and help them with learning pro, Teach as a member of a teaching team within an established programme of study.'
+          descriptionItems: [
+            'Supporting lecturers in preparing and delivering lectures, seminars, and tutorials. This includes aiding in creating lesson plans, presentations, and teaching materials.',
+            'Providing assistance to students by conducting consultation hours, offering guidance on coursework, assignments, and projects, and addressing their academic queries.',
+            'Offering guidance and support to students regarding academic goals, career paths, and program requirements. Acting as a mentor to students, providing advice and encouragement',
+            'Engaging in ongoing professional development opportunities, such as attending workshops, conferences, or pursuing advanced degrees to enhance teaching skills and subject knowledge.',
+            'Supporting research activities by conducting literature reviews, gathering data, organizing research materials, or assisting in experiments, depending on the academic field.',
+            'Assisting in grading assignments, exams, and other academic assessments. Ensuring fairness and consistency in assessment practices based on established guidelines.',
+          ],
+          employmentStatus: "Contract",
+          workStatus: "On-Site" 
         },
         {
           title: 'Photographer',
@@ -89,7 +138,17 @@ new Vue({
           companyLink: '#',
           startDate: '2016/04',
           endDate: '2016/09',
-          description: 'Take photos of clients or objects in studio environments.'
+          descriptionItems: [
+            'Conducting various photography sessions including portrait, product, fashion, or commercial shoots within the studio.',
+            'Setting up and adjusting studio lighting equipment to achieve the desired mood, ambiance, and effects for each shoot.',
+            'Operating and maintaining photography equipment including cameras, lenses, tripods, and other accessories.',
+            'Collaborating with clients to understand their specific needs, providing guidance on poses, themes, and styles for the shoot.',
+            'Organizing and managing photography sessions, maintaining an efficient workflow, and meeting deadlines.',
+            'Ensuring proper care, maintenance, and cleanliness of photography equipment and studio spaces.',
+
+          ],
+          employmentStatus: "Contract",
+          workStatus: "On-Site" 
         },
         // Add other experiences as needed...
       ],
@@ -99,56 +158,56 @@ new Vue({
           validationLink: 'https://university.cpanel.net/certificate/290f-0d9e-c6ab-4dad',
           startDate: '2020',
           endDate: 'Now',
-          description: 'From cPanel University'
+          description: 'cPanel University'
         },
         {
           title: 'cPanel & WHM Sales Professional (CPSP)',
           validationLink: 'https://university.cpanel.net/certificate/c886-aa62-89f1-53e1',
           startDate: '2021',
           endDate: 'Now',
-          description: 'From cPanel University'
+          description: 'cPanel University'
         },
         {
           title: 'cPanel & WHM Administrator Certification (CWA)',
           validationLink: 'https://university.cpanel.net/certificate/dc80-bac7-81fa-28e9',
           startDate: '2020',
           endDate: 'Now',
-          description: 'From cPanel University'
+          description: 'cPanel University'
         },
         {
           title: 'cPanel & WHM System Administrator I (CWSA-1)',
           validationLink: 'https://university.cpanel.net/certificate/091d-3432-a27e-47c6',
           startDate: '2020',
           endDate: 'Now',
-          description: 'From cPanel University'
+          description: 'cPanel University'
         },
         {
           title: 'cPanel & WHM Sales Professional (CPSP)',
           validationLink: 'https://university.cpanel.net/certificate/c886-aa62-89f1-53e1',
           startDate: '2021',
           endDate: 'Now',
-          description: 'From cPanel University'
+          description: 'cPanel University'
         },
         {
           title: 'cPanel & WHM Sales Professional (CPSP)',
           validationLink: 'https://university.cpanel.net/certificate/c886-aa62-89f1-53e1',
           startDate: '2021',
           endDate: 'Now',
-          description: 'From cPanel University'
+          description: 'cPanel University'
         },
         {
           title: 'cPanel & WHM System Administrator II (CWSA-2)',
           validationLink: 'https://university.cpanel.net/certificate/ce77-1003-fc3c-f92c',
           startDate: '2020',
           endDate: 'Now',
-          description: 'From cPanel University'
+          description: 'cPanel University'
         },
         {
           title: 'LiteSpeed Certification',
           validationLink: 'https://university.cpanel.net/certificate/c552-b77a-6697-ddcd',
           startDate: '2021',
           endDate: 'Now',
-          description: 'From cPanel University'
+          description: 'cPanel University'
         },
         {
           title: 'Imunify360 Certification',
@@ -161,21 +220,21 @@ new Vue({
           validationLink: 'https://university.cpanel.net/certificate/042d-7426-83dd-2ccf',
           startDate: '2023',
           endDate: 'Now',
-          description: 'From cPanel University'
+          description: 'cPanel University'
         },
         {
           title: 'JetBackup 5 Certification',
           validationLink: 'https://university.cpanel.net/certificate/0e8c-2ac3-ae9b-a1ea',
           startDate: '2023',
           endDate: 'Now',
-          description: 'From cPanel University'
+          description: 'cPanel University'
         },
         {
           title: 'cPanel Certified Partner Accreditation (CPCP)',
           validationLink: 'https://university.cpanel.net/certificate/1ddf-2e94-4bdf-3673',
           startDate: '2023',
           endDate: 'Now',
-          description: 'From cPanel University'
+          description: 'cPanel University'
         },
         // Add other certifications as needed...
       ],
@@ -205,12 +264,13 @@ new Vue({
       ],
       interestClass: 'inline-flex mr-2 text-xs font-semibold text-opacity-70 text-black border px-1 rounded border-purple-100 bg-purple-50 dark:border-gray-900 dark:bg-gray-600'
     },
+    
     methods: {
       getInterestClasses(index) {
         return {
           [this.interestClass]: true
         };
-      }
+      },
     }
   });
   
--- readme.md
+++ readme.md
@@ -1,5 +1,50 @@
+# Resume Website
+---
+This is the repository for my personal resume website, [resume.masdzub.com](https://resume.masdzub.com/). The website is built using HTML, CSS, JavaScript, and leverages Tailwind CSS for styling. Vue.js is utilized for managing variables, which can be edited in `assets/data.js`.
 
-For Variable can edited at `assets/data.js`
+## Overview
 
+This project is a personalized version of a resume website, inspired by [karimhossenbux/resume](https://github.com/karimhossenbux/resume), but using a different tech stack. It provides an interactive and responsive platform to showcase my resume and skills.
 
-this website from https://github.com/karimhossenbux/resume but using html, css, js also vue 
+## Technologies Used
+
+- **HTML**: Structuring the content of the website.
+- **CSS & Tailwind CSS**: Styling and design of the website, with the use of Tailwind CSS utility classes.
+- **JavaScript**: Adding interactivity and functionality.
+- **Vue.js**: Utilized for managing variables and enhancing the website's capabilities.
+
+## Getting Started
+
+To run this project locally, follow these steps:
+
+1. Clone this repository:
+
+   ```bash
+   git clone https://github.com/masdzub/cv.masdzub.com.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd resume
+   ```
+
+3. Edit the variables in `assets/data.js` according to your preferences.
+
+4. Open the `index.html` file in your preferred browser or set up a local server.
+
+## Customization
+
+The main configuration and customization can be done by modifying the variables in `assets/data.js`. This file contains key information used throughout the website, including personal details, experience, education, skills, and more.
+
+## Credits
+
+- Original inspiration and reference: [karimhossenbux/resume](https://github.com/karimhossenbux/resume)
+- Tailwind CSS: [Tailwind CSS](https://tailwindcss.com/)
+- Vue.js: [Vue.js](https://vuejs.org/)
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
